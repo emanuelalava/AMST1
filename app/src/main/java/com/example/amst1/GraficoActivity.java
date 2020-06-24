d9c0883a5a9e46ac0d50d2831b6f9a07ca4eb577
Winter Alava <walava@espol.edu.ec>
2020-06-24 00:03:06 -0500

    Se creo el grafico activity usando las dependencias de MPAndroidChart

---
+++ app/src/main/java/com/example/amst1/GraficoActivity.java
@@ -0,0 +1,38 @@
+package com.example.amst1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
+public class GraficoActivity extends AppCompatActivity {
+    private LineChart lineChart;
+    private LineDataSet lineDataSet;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grafico);
+        lineChart = (LineChart) findViewById(R.id.lineChart);
+
+        // Creamos un set de datos aleatorios de prueba
+        ArrayList<Entry> lineEntries = new ArrayList<Entry>();
+        for (int i = 0; i<11; i++){
+            float y = (int) (Math.random() * 4) + 1;
+            lineEntries.add(new Entry((float) i,(float)y));
+        }
+
+        // Se vinculan los datos al dataset
+        lineDataSet = new LineDataSet(lineEntries, "Estadisticas Randoms de AMST");
+
+        // Se vincula el dataset con LineData y estas con el gráfico
+        LineData lineData = new LineData();
+        lineData.addDataSet(lineDataSet);
+        lineChart.setData(lineData);
+    }
+}
--- app/src/main/java/com/example/amst1/MainActivity.java
+++ app/src/main/java/com/example/amst1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.amst1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void toGrafico(View view){
+        Intent i = new Intent(MainActivity.this,GraficoActivity.class);
+        startActivity(i);
+    }
+
+    public void toVideoView(View view){
+        Intent i = new Intent(MainActivity.this,VideoViewActivity.class);
+        startActivity(i);
+    }
+
+//    public void toCalendar(View view){
+//        Intent i = new Intent(MainActivity.this,XXXXXX.class);
+//        startActivity(i);
+//    }
+
+//    public void toMaps(View view){
+//        Intent i = new Intent(MainActivity.this,XXXXXXXX.class);
+//        startActivity(i);
+//    }
+
+//    public void toBotonFlotante(View view){
+//        Intent i = new Intent(MainActivity.this,XXXXXXX.class);
+//        startActivity(i);
+//    }
 }
