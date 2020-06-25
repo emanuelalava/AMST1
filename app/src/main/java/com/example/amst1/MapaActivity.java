d9bb684fcf362f3e585fd83a164eece13a643a1d
Adriana Murrieta <avmurrie@espol.edu.ec>
2020-06-24 19:28:17 -0500

    Cambios en el mapa

--- app/src/main/java/com/example/amst1/MapaActivity.java
+++ app/src/main/java/com/example/amst1/MapaActivity.java
@@ -1,7 +1,11 @@
 package com.example.amst1;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -38,9 +42,30 @@ public class MapaActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //Añade un marcador en Ecuador
+        LatLng ec = new LatLng(-2.19616, -79.88621);
+        mMap.addMarker(new MarkerOptions().position(ec).title("Guayquil-Ecuador"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ec, 16));
+        //Habilita el zoom
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        //Habilita detectar la ubicación
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+       mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+
+
+
     }
 }
